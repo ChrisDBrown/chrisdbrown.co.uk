dead5b1892828b80669c93995eacc09d99c18c2a
Chris D Brown <owner@chrisdbrown.co.uk>
2020-02-05 21:37:54 +0000

    feat: gatsby-universal

--- scripts/socialIcons.js
+++
@@ -1,9 +0,0 @@
-hexo.extend.tag.register('socialIcons', function(args){
-  return `
-  <p class="social-icons">
-    <a href="mailto:owner@chrisdbrown.co.uk"><i class="icon-email"></i></a>
-    <a href="https://github.com/ChrisDBrown" target="_blank"><i class="icon-github"></i></a>
-    <a href="https://www.linkedin.com/in/chrisdeebrown" target="_blank"><i class="icon-linkedin"></i></a>
-    <a href="https://twitter.com/ChrisDeeBrown" target="_blank"><i class="icon-twitter"></i></a>
-  </p>`;
-});
