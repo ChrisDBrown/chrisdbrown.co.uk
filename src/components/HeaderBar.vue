1a503442122073a6c8619cdef504830d7d7acb27
ChrisDBrown <owner@chrisdbrown.co.uk>
2023-10-30 23:58:52 +0000

    feat: Style content

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,12 @@
+<script setup>
+import NavigationBlock from "./NavigationBlock.vue";
+</script>
+
+<template>
+  <header class="flex justify-between">
+    <a href="/">
+      <h1 class="text-3xl link">chris d brown</h1>
+    </a>
+    <NavigationBlock />
+  </header>
+</template>
