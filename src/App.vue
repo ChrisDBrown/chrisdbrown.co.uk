327038a38c98d8154be1c9521a372df59545ffce
ChrisDBrown <owner@chrisdbrown.co.uk>
2025-04-05 11:31:55 +0100

    feat: Update eslint and tailwindcss major versions

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import HeaderBar from "./components/HeaderBar.vue";
 </script>
 
 <template>
-  <div class="p-6 sm:p-12 w-full">
+  <div class="w-full p-6 sm:p-12">
     <HeaderBar />
     <router-view />
   </div>
