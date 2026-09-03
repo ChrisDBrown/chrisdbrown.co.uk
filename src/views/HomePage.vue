55399bac56753ca1043aa4d3267917d43d00cebb
ChrisDBrown <owner@chrisdbrown.co.uk>
2026-09-03 11:27:36 +0100

    feat: Add freelance work

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -6,19 +6,25 @@ import StyledLink from "../components/StyledLink.vue";
   <main class="prose">
     <h2>engineering lead based in glasgow, scotland</h2>
     <p>
-      With 12 years industry experience under my belt, I’ve worked across all
+      With 12 years industry experience under my belt, I've worked across all
       areas of web development; creating cohesive, multi-discipline projects
       both as part of a team and as a solo developer. Working in companies where
-      I can make a tangible impact through my work, I’ve been involved in the
-      fashion, music and entertainment verticals from an eComms perspective, and
-      I’ve pushed to create new solutions across the UK’s fintech and medtech
-      sectors.
+      I can make a tangible impact through my work, I've been involved in the
+      fashion, music and entertainment verticals from an eCommerce perspective,
+      and I've pushed to create new solutions across the UK's fintech and
+      medtech sectors.
     </p>
     <p>
-      Complex projects are my bread and butter. Currently I'm working as a
+      Complex projects are my bread and butter. I'm currently working as a
       full-stack Lead Engineer for an industry-leading SaaS in the Architectural
       world, helping accelerate the modelling process for millions of users at
       all scales.
+    </p>
+    <p>
+      As a freelancer, I've worked with global entertainment brands on marketing
+      and eCommerce solutions for titles like Godzilla and Akira. Working on
+      home video and theatrical releases, I've created bespoke viewing sites,
+      festival calendars and streaming events with attendees across Europe.
     </p>
     <p>
       I'm always looking for new opportunities. If you think I'd fit in with
@@ -77,25 +83,6 @@ import StyledLink from "../components/StyledLink.vue";
         <StyledLink href="https://chrisdbrown.itch.io/"
           >and all the rest</StyledLink
         >
-      </li>
-    </ul>
-    <h2>other bits</h2>
-    <ul>
-      <li>
-        <StyledLink href="https://github.com/ChrisDBrown/vue-tailwind-template"
-          >vue + tailwind</StyledLink
-        >
-        &amp;
-        <StyledLink href="https://github.com/ChrisDBrown/vue-tailwind-template"
-          >symfony</StyledLink
-        >
-        opinionated templates
-      </li>
-      <li>
-        <StyledLink href="https://github.com/ChrisDBrown/bskymarks"
-          >bskymarks</StyledLink
-        >
-        bookmarking tool for bluesky
       </li>
     </ul>
   </main>
